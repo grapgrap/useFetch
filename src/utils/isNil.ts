f99aa7b51cae30ba537d9c5891fe3c4f698c24eb
grapgrap <fussaep@gmail.com>
2021-10-23 17:42:00 +0900

    fix: Add return type on isNil to type narrow

--- src/utils/isNil.ts
+++ src/utils/isNil.ts
@@ -1,1 +1,3 @@
-export const isNil = (value: unknown) => value === undefined || value === null;
+export function isNil(value: unknown): value is null | undefined {
+  return value === undefined || value === null;
+}
